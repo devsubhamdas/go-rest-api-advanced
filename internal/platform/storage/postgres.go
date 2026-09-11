e122eee7582f0061643d0f4afdbcb8d104918989
devsubhamdas <das198subham@gmail.com>
2026-09-11 13:16:33 +0530

    enable translate error

--- internal/platform/storage/postgres.go
+++ internal/platform/storage/postgres.go
@@ -22,7 +22,9 @@ func NewPostgres(cfg *config.Config) (*gorm.DB, error) {
 		cfg.DB_SSLMODE,
 	)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		TranslateError: true,
+	})
 	if err != nil {
 		return nil, err
 	}
