81e2a9f3a82084b9e592aea1437dec8e8309314d
devsubhamdas <das198subham@gmail.com>
2026-09-14 14:05:19 +0530

    update errorsx

--- internal/platform/errorsx/errorsx.go
+++ internal/platform/errorsx/errorsx.go
@@ -3,6 +3,8 @@ package errorsx
 import "errors"
 
 var (
+	ErrInvalidID          = errors.New("invalid id")
+	ErrInvalidEmail       = errors.New("invalid email")
 	ErrEmailAlreadyExists = errors.New("email already exists")
 	ErrDuplicateKey       = errors.New("duplicate key error")
 	ErrNotFound           = errors.New("not found")
