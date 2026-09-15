9ca146b67e1551ed22814a62502163c0cd58e2a2
devsubhamdas <das198subham@gmail.com>
2026-09-15 12:14:07 +0530

    remove return type user router.go

--- internal/user/router.go
+++ internal/user/router.go
@@ -2,12 +2,10 @@ package user
 
 import "net/http"
 
-func RegisterRoutes(mux *http.ServeMux, h *Handler) *http.ServeMux {
+func RegisterRoutes(mux *http.ServeMux, h *Handler) {
 	mux.HandleFunc("POST /api/users", h.CreateUser)
 	mux.HandleFunc("PATCH /api/users", h.UpdateUser)
 	mux.HandleFunc("DELETE /api/users/{id}", h.DeleteUser)
 	mux.HandleFunc("GET /api/users/{id}", h.GetUserByID)
 	mux.HandleFunc("GET /api/users", h.GetUsers)
-
-	return mux
 }
